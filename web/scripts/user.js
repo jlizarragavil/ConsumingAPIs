bf0afddb28cdd107eb2fd815c075c8539e6e1503
Juan <72524993+jdlopez-dev@users.noreply.github.com>
2021-10-19 16:37:41 +0200

    refactor: :recycle: change promise to asyn await and refactor code

--- web/scripts/user.js
+++ web/scripts/user.js
@@ -1,23 +1,29 @@
-let url = "http://localhost:8080/userTable/all";
-llamada(url);
+const url = "http://localhost:8080/userTable/all";
+
+printTable(url);
+
 function callApi() {
-	orderBy = document.getElementById("usersCombo").value;
-  let url = "http://localhost:8080/userTable/all/sort/"+orderBy;
-  llamada(url);
+  orderBy = document.getElementById("usersCombo").value;
+  const urlOrderby = `${url}/sort/${orderBy}`;
+  printTable(urlOrderby);
 }
-function calearFilters() {
-  let url = "http://localhost:8080/userTable/all";
-  llamada(url);
+
+function clearFilters() {
+  printTable(url);
 }
-function llamada(url) {
+
+async function getData(url) {
+  const response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
+function printTable(url) {
   if (document.contains(document.getElementById("tbody"))) {
     document.getElementById("tbody").remove();
   }
 
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
+  getData(url)
     .then(function (obj) {
       var table = document.getElementById("tabla");
       var tbody = document.createElement("tbody");
@@ -26,11 +32,11 @@ function llamada(url) {
       for (let i = 0; i < obj.length; i++) {
         console.log(
           "Table data: " +
-            obj[i]["name"] +
-            ", " +
-            obj[i]["id"] +
-            ", " +
-            obj[i]["age"]
+          obj[i]["name"] +
+          ", " +
+          obj[i]["id"] +
+          ", " +
+          obj[i]["age"]
         );
         var hilera = document.createElement("tr");
         for (let j = 0; j < 3; j++) {
